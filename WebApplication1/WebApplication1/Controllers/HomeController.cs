ce49373f2e44ecda55b2738020f3995fcd0800fd
Emil Sadullazada <tu4yu0ae2@aztu.local>
2022-06-24 19:43:15 +0400

    task

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+using WebApplication1.ViewModel;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HomeVm homeVm = new HomeVm
+            {
+                Sliders = await _context.Sliders.ToListAsync(),
+            };
+            return View(homeVm);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Slider.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+
+    }
+}
